68a75a43d07881afa8bec987fd48c4d9cde84604
TiarleiNoeremberg <tiarlei.com@gmail.com>
2024-11-11 14:15:08 -0300

    POST insert Controller e Service

--- src/main/java/com/tiarlei/dscommerce/controllers/ProductController.java
+++ src/main/java/com/tiarlei/dscommerce/controllers/ProductController.java
@@ -5,10 +5,7 @@ import com.tiarlei.dscommerce.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,11 +20,18 @@ public class ProductController {
     public ProductDTO findById(@PathVariable Long id) {
         //ProductDTO dto = service.findById(id);
         //return dto;
-        return service.findById(id);    //Esta linha oferece uma opcao mais simplificada do comando acima.
+        return service.findById(id);    //Esta linha oferece uma opcao mais simplificada do comando comentado acima.
     }
 
     @GetMapping
     public Page<ProductDTO> findAll(Pageable pageable) {
         return service.findAll(pageable);
+    }
+
+    @PostMapping
+    public ProductDTO insert(@RequestBody ProductDTO dto) {
+        //dto = service.insert(dto);
+        //return dto;
+        return service.insert(dto);
     }
 }
