1500830dbef77604105f9c6866074713837f7003
TiarleiNoeremberg <tiarlei.com@gmail.com>
2024-11-11 14:30:16 -0300

    Implementacao resposta 201 para sucesso na insercao

--- src/main/java/com/tiarlei/dscommerce/controllers/ProductController.java
+++ src/main/java/com/tiarlei/dscommerce/controllers/ProductController.java
@@ -5,8 +5,11 @@ import com.tiarlei.dscommerce.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -17,21 +20,28 @@ public class ProductController {
     private ProductService service;
 
     @GetMapping(value = "/{id}")
-    public ProductDTO findById(@PathVariable Long id) {
+    public ResponseEntity<ProductDTO> findById(@PathVariable Long id) {
         //ProductDTO dto = service.findById(id);
         //return dto;
-        return service.findById(id);    //Esta linha oferece uma opcao mais simplificada do comando comentado acima.
+        //return service.findById(id);    //Esta linha oferece uma opcao mais simplificada do comando comentado acima.
+        //codigo acima era anterior ao ResponseEntity
+        ProductDTO dto = service.findById(id);
+        return ResponseEntity.ok(dto);
     }
 
     @GetMapping
-    public Page<ProductDTO> findAll(Pageable pageable) {
-        return service.findAll(pageable);
+    public ResponseEntity<Page<ProductDTO>> findAll(Pageable pageable) {
+        Page<ProductDTO> dto = service.findAll(pageable);
+        return ResponseEntity.ok(dto);
     }
 
     @PostMapping
-    public ProductDTO insert(@RequestBody ProductDTO dto) {
+    public ResponseEntity<ProductDTO> insert(@RequestBody ProductDTO dto) {
         //dto = service.insert(dto);
         //return dto;
-        return service.insert(dto);
+        dto = service.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(dto);
     }
 }
