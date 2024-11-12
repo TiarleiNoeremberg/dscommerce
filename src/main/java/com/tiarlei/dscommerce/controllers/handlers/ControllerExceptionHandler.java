a69ce94b49fbc12bbdc9a461356c1f3a8951e782
TiarleiNoeremberg <tiarlei.com@gmail.com>
2024-11-12 15:10:11 -0300

    Exception handling

--- src/main/java/com/tiarlei/dscommerce/controllers/handlers/ControllerExceptionHandler.java
+++ src/main/java/com/tiarlei/dscommerce/controllers/handlers/ControllerExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.tiarlei.dscommerce.controllers.handlers;
 
 import com.tiarlei.dscommerce.dto.CustomError;
+import com.tiarlei.dscommerce.services.exceptions.DatabaseException;
 import com.tiarlei.dscommerce.services.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
@@ -15,6 +16,13 @@ public class ControllerExceptionHandler {
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<CustomError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
         HttpStatus status = HttpStatus.NOT_FOUND;
+        CustomError err = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(DatabaseException.class)
+    public ResponseEntity<CustomError> database(DatabaseException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.BAD_REQUEST;
         CustomError err = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
         return ResponseEntity.status(status).body(err);
     }
