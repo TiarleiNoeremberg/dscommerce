1e8936df4a64e85b87556c71458e337827a9645c
TiarleiNoeremberg <tiarlei.com@gmail.com>
2025-01-12 14:00:18 -0300

    UserDetails, GrantedAuthority, not tested

--- src/main/java/com/tiarlei/dscommerce/entities/User.java
+++ src/main/java/com/tiarlei/dscommerce/entities/User.java
@@ -1,19 +1,21 @@
 package com.tiarlei.dscommerce.entities;
 
 import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDate;
 import java.util.*;
 
 @Entity
 @Table(name = "tb_user")
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
-
+   
     @Column(unique = true)
     private String email;
     private String phone;
@@ -81,8 +83,38 @@ public class User {
         this.birthDate = birthDate;
     }
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles;
+    }
+
     public String getPassword() {
         return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 
     public void setPassword(String password) {
